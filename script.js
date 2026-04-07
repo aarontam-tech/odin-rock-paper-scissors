5328a313bdb7667574d9cbcf9676592276be5994
Aaron <247822488+aarontam-tech@users.noreply.github.com>
2026-04-06 22:14:39 -0400

    Add play function for a single round
    The logic for a single round will support logic for multiple rounds.

--- script.js
+++ script.js
@@ -16,3 +16,43 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Please enter choice: 'rock' 'paper' 'scissors'.");
 }
+
+function playRound(computerChoice, humanChoice) {
+  let result = 0;
+  humanChoice = humanChoice.toLowerCase();
+
+  if(computerChoice == "rock") {
+    if(humanChoice == "scissors") {
+      result = -1;
+    } else if(humanChoice == "paper") {
+      result = 1;
+    }
+  } else if(computerChoice == "paper") {
+    if(humanChoice == "rock") {
+      result = -1;
+    } else if(humanChoice == "scissors") {
+      result = 1;
+    }
+  } else {
+    if(humanChoice == "paper") {
+      result = -1;
+    } else if(humanChoice == "rock") {
+      result = 1;
+    }
+  }
+
+  if(result == 1) {
+    humanScore += 1;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+  } else if(result == -1) {
+    computerScore += 1;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+  } else {
+    console.log(`You tie! You both chose ${humanChoice}.`);
+  }
+}
+
+let computerChoice = getComputerChoice(),
+  humanChoice = getHumanChoice();
+
+playRound(computerChoice, humanChoice);
