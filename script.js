6d78bd28dcf1d385ed4957daf7258c8529c40dfe
Aaron <247822488+aarontam-tech@users.noreply.github.com>
2026-04-06 22:49:16 -0400

    Add play function for multiple rounds
    The game must be multiple rounds.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let computerScore = 0,
-  humanScore = 0;
-
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random()*3);
 
@@ -17,42 +14,61 @@ function getHumanChoice() {
   return prompt("Please enter choice: 'rock' 'paper' 'scissors'.");
 }
 
-function playRound(computerChoice, humanChoice) {
-  let result = 0;
-  humanChoice = humanChoice.toLowerCase();
+function playGame() {
+  let rounds = 5,
+    computerScore = 0,
+    humanScore = 0,
+    computerChoice,
+    humanChoice;
 
-  if(computerChoice == "rock") {
-    if(humanChoice == "scissors") {
-      result = -1;
-    } else if(humanChoice == "paper") {
-      result = 1;
+  function playRound(computerChoice, humanChoice) {
+    let result = 0;
+    humanChoice = humanChoice.toLowerCase();
+
+    if(computerChoice == "rock") {
+      if(humanChoice == "scissors") {
+        result = -1;
+      } else if(humanChoice == "paper") {
+        result = 1;
+      }
+    } else if(computerChoice == "paper") {
+      if(humanChoice == "rock") {
+        result = -1;
+      } else if(humanChoice == "scissors") {
+        result = 1;
+      }
+    } else {
+      if(humanChoice == "paper") {
+        result = -1;
+      } else if(humanChoice == "rock") {
+        result = 1;
+      }
     }
-  } else if(computerChoice == "paper") {
-    if(humanChoice == "rock") {
-      result = -1;
-    } else if(humanChoice == "scissors") {
-      result = 1;
-    }
-  } else {
-    if(humanChoice == "paper") {
-      result = -1;
-    } else if(humanChoice == "rock") {
-      result = 1;
+
+    if(result == 1) {
+      humanScore += 1;
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    } else if(result == -1) {
+      computerScore += 1;
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    } else {
+      console.log(`You tie! You both chose ${humanChoice}.`);
     }
   }
 
-  if(result == 1) {
-    humanScore += 1;
-    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-  } else if(result == -1) {
-    computerScore += 1;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+  for(let i = 0; i < rounds; i++) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+    playRound(computerChoice, humanChoice);
+  }
+
+  if(computerScore < humanScore) {
+    console.log("The winner is you.");
+  } else if(computerScore > humanScore) {
+    console.log("The winner is the computer.");
   } else {
-    console.log(`You tie! You both chose ${humanChoice}.`);
+    console.log("The game ends in a draw.");
   }
 }
 
-let computerChoice = getComputerChoice(),
-  humanChoice = getHumanChoice();
-
-playRound(computerChoice, humanChoice);
+playGame();
